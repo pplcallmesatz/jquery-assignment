07cd737d0b3e1f9791e78d6af32b2821be3325cc
Satheesh Kumar <satheeshssk@icloud.com>
2017-05-31 14:22:25 +0530

    Forms

---
+++ forms/main.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+    $("#one").on('input',function(){
+       var node = $(this);
+    node.val(node.val().replace(/[^a-z A-Z]/g,'') ); 
+    })
+    
+     $("#two").on('input',function(){
+       var node = $(this);
+    node.val(node.val().replace(/[^0-9]/g,'') ); 
+    })
+     $("#three").on('input',function(){
+       var node = $(this);
+    node.val(node.val().replace(/^[a-zA-Z0-9]/g,'') ); 
+    })
+    
+});
