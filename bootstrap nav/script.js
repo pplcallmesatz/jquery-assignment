3f41f7c3695c6b80538baeb2b2d968c81c4d69f5
Satheesh Kumar <satheeshssk@icloud.com>
2017-11-17 10:05:28 +0530

    Added Bootstrap plugin

---
+++ bootstrap nav/script.js
@@ -0,0 +1,128 @@
+
+
+$(document).ready(function(){
+     general();
+});
+$(window).on("load", function(){
+//     general();
+});
+$(window).resize(function(){
+     general();
+});
+
+
+function expansion(){
+ 
+    
+    
+    
+    
+}
+
+function contraction(){
+    
+}
+
+function general(){
+    
+    //whole width
+    var navWholeWidth = $(".nav").width();   
+//    console.log("total width : ", navWholeWidth );
+    
+    // Nav pills total
+    var navTotalPills = $(".nav-item");
+    var navCounter = 0;
+    navTotalPills.each(function(){
+//        console.log("d")
+     var navPillIndividualWidth = $(this).width();
+        navCounter += navPillIndividualWidth;
+        var l =$(".dropdown-menu .dropdown-item").length;
+//        console.log(navCounter);
+        if ((navCounter + 50) > navWholeWidth) {
+//            console.log("e")
+$(".nav .nav-item").last().prev().removeClass("nav-item").detach().prependTo(".dropdown-menu").addClass("dropdown-item");
+        }
+        else if(l > 0){
+            $(".dropdown-menu .dropdown-item").each(function(){
+                elseFunctionTotalWidth = 0;
+                navTotalPills.each(function(){
+                    var ww = $(this).width();
+                    elseFunctionTotalWidth += ww;
+                    
+                    
+                    
+                }
+                
+                
+                var dW = $(this).width();
+                
+                
+                
+                var pilWid = navCounter;
+                pilWid += dW;
+                
+                if ((pilWid + 150) < navWholeWidth){
+                    console.log("navCounter",navCounter ,"PillWidth", pilWid, "total width", navWholeWidth);
+                    $(this).removeClass("dropdown-item").detach().insertBefore(".dropdown").addClass(".nav-item");
+                }
+            });
+            
+        }
+        else {
+            
+//            console.log("f")
+            
+        }
+        
+         
+//        console.log("navCounter ",navCounter);
+        var l =$(".dropdown-menu .dropdown-item").length;
+
+        
+        $(".navInside").text(l);
+        
+    });
+    
+    
+    
+    
+//    console.log("nav item total ",navCounter);
+    
+    
+    // Navpills with buffer
+//    var navCounterBuffer = navCounter + 120;
+//        console.log("buffer ",navCounterBuffer);
+
+    // Count dropdown menu
+//    var navItemCount = $(".nav .nav-item");
+//    var dropDownItemCountdisplay = $(".navInside");
+//    var dropDown = $(".dropdown");
+//    var dropDownMenu = $(".dropdown-menu");
+//    var dropDownItem = $(".dropdown-menu .dropdown-item");
+//    var dropDownCount = dropDownItem.length;
+//    console.log(dropDownCount);
+//    dropDownItemCountdisplay.text(dropDownCount);
+    
+    
+//    var pillsWidth = navCounter;
+//    navTotalPills.each(function(){
+        
+//        console.log("pills width", pillsWidth);
+//        if (pillsWidth > navWholeWidth) {
+//            var currentWidth = $(this).width();
+//            pillsWidth -= currentWidth;
+            
+//            var lastPrev = $(".nav-item").last().prev(),
+//                lastPrevWidth = lastPrev.width();
+//            pillsWidth -= lastPrevWidth;
+//            console.log(lastPrevWidth);
+//                    lastPrev.detach().appendTo(dropDownMenu);
+
+           
+//        }
+        
+//    });
+//     return pillsWidth;
+      
+   
+}
